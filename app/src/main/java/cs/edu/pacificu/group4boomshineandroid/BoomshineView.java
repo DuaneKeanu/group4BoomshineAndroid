534bbbbf97aa67b32be6390a92c2aa16e2bf59c4
jpduong12 <jpduong12@gmail.com>
2021-05-03 23:35:09 -0700

    Added new jar file

--- app/src/main/java/cs/edu/pacificu/group4boomshineandroid/BoomshineView.java
+++ app/src/main/java/cs/edu/pacificu/group4boomshineandroid/BoomshineView.java
@@ -3,7 +3,11 @@ package cs.edu.pacificu.group4boomshineandroid;
 import android.content.Context;
 import android.view.View;
 
+import group4Boomshine.BoomshinePresenter;
+
 public class BoomshineView extends View {
+
+  private BoomshinePresenter mPresenter;
 
   public BoomshineView (Context context) {
     super (context);
